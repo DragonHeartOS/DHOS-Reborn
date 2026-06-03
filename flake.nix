db11ae7dcac7e9f82a82baebb9f666fc3cd88ac5
Slendi <slendi@socopon.com>
2026-06-03 20:36:15 +0300

    CI: Hopefully fix CI and optimize
    Signed-off-by: Slendi <slendi@socopon.com>

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
         pkgs = import nixpkgs { inherit system; };
         llvm = pkgs.llvmPackages_21;
         limineClang = pkgs.writeShellScriptBin "limine-clang" ''
-          exec ${llvm.clang}/bin/clang -target x86_64-apple-darwin "$@"
+          exec ${llvm.clang}/bin/clang "$@"
         '';
         limineSrc = pkgs.fetchFromGitHub {
           owner = "limine-bootloader";
@@ -24,10 +24,14 @@
           rev = "5be26a73d7b7b4d4477d18be94e1d16e615adf56";
           hash = "sha256-lBPx5B3yiuWC+CiaygsOwCWKTEnLU2Wv/DE+msGXM6w=";
         };
-        dhos = pkgs.stdenv.mkDerivation {
+        dhos = pkgs.stdenvNoCC.mkDerivation {
           pname = "dhos";
           version = "0.1";
           src = pkgs.lib.cleanSource ./.;
+
+          postPatch = ''
+            patchShebangs Util/*.sh
+          '';
 
           postUnpack = ''
             cp -R ${limineSrc} "$sourceRoot/limine"
@@ -42,6 +46,7 @@
             python3
             xorriso
             llvm.clang-unwrapped
+            llvm.bintools
             llvm.lld
           ];
 
@@ -49,8 +54,14 @@
             cmake -G Ninja -B Build \
               -DCMAKE_BUILD_TYPE=Debug \
               -DCMAKE_TOOLCHAIN_FILE=cmake/x86_64-elf-clang.cmake \
-              -DCMAKE_C_COMPILER=clang \
-              -DCMAKE_CXX_COMPILER=clang++
+              -DCMAKE_C_COMPILER=${llvm.clang-unwrapped}/bin/clang \
+              -DCMAKE_CXX_COMPILER=${llvm.clang-unwrapped}/bin/clang++ \
+              -DCLANG=${llvm.clang-unwrapped}/bin/clang \
+              -DCLANGXX=${llvm.clang-unwrapped}/bin/clang++ \
+              -DLLD=${llvm.lld}/bin/ld.lld \
+              -DNASM=${pkgs.nasm}/bin/nasm \
+              -DCMAKE_AR=llvm-ar \
+              -DCMAKE_RANLIB=llvm-ranlib
           '';
 
           buildPhase = ''
@@ -68,7 +79,7 @@
         packages.default = dhos;
 
         devShells.default =
-          pkgs.mkShell {
+          pkgs.mkShellNoCC {
             packages =
               with pkgs;
               [
