a294e819559896a834def98d6acead77597c490d
Slendi <slendi@socopon.com>
2026-06-15 03:19:58 +0300

    Util/Run: Add smp count and headless environs
    Signed-off-by: Slendi <slendi@socopon.com>

--- Util/Run.sh
+++ Util/Run.sh
@@ -2,6 +2,10 @@
 
 SYSTEM='QEMU'
 
+# Configuration
+SMP_COUNT="${SMP_COUNT:-4}"
+HEADLESS="${HEADLESS:-0}"
+
 if [ "$1" = 'Bochs' ] ; then
     echo 'Bochs not yet implemented!'
     exit 1
@@ -16,9 +20,25 @@ has_kvm() {
     [ -e /dev/kvm ] && [ -r /dev/kvm ] && [ -w /dev/kvm ]
 }
 
+is_headless() {
+    case "${HEADLESS,,}" in
+        1|true|yes|on)
+            return 0
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
 if [ "$SYSTEM" = 'QEMU' ] ; then
     QEMU_BIN='qemu-system-x86_64'
-    QEMU_ARGS="-m 1G -serial stdio -smp 4"
+    QEMU_ARGS="-m 1G -serial stdio -smp ${SMP_COUNT}"
+
+    if is_headless ; then
+        echo 'Running headless'
+        QEMU_ARGS="$QEMU_ARGS -display none -monitor none"
+    fi
 
     if has_kvm ; then
         echo 'Using KVM acceleration'
@@ -37,5 +57,5 @@ if [ "$SYSTEM" = 'QEMU' ] ; then
         exit 1
     fi
 
-    exec $QEMU_BIN $QEMU_ARGS
+    exec "$QEMU_BIN" $QEMU_ARGS
 fi
